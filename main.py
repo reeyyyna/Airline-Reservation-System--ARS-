be2e93c49986158b1357fd118838ea443276a9d3
Quennie Ann Soberano <umsworld16@gmail.com>
2023-09-01 10:12:15 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Main Program
+try: 
+	while True:
+		print("\n\n\t\t\t==== AIRLINE RESERVATION SYSTEM ====\n\n")
+		print("\t\t\t1. Display Seat Map")
+		print("\t\t\t2. Reserve a Seat")
+		print("\t\t\t3. Cancel Reservation")
+		print("\t\t\t4. Display Available Seats")
+		print("\t\t\t5. Display Flight Information")
+		print("\t\t\t6. Refresh System and CSV Data")
+		print("\t\t\t7. Exit")
+
+		choice = input("\nEnter your choice: ")
+
+except:
+	print("\nThe system execution is done.")
